b3dcf8cc2833a505809dd84825ab5c2b485bdd47
Max Fry <maxcfry@outlook.com>
2021-09-22 14:42:35 +0100

    First red green testing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-  # it "returns true when bike is >= 1" do
-  #   expect(DockingStation.new.check("1")).to(eq(true))
-  # end
+  it "returns true when bike is >= 1" do
+     expect(DockingStation.new.release_bike).to(eq(nil))
+  end
 end
+
+
