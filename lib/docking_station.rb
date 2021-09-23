e784d0ae0b822c01ea84e6f3f84c703a7f78000c
Anthony Osmaston <anthonyosmaston@Anthonys-MacBook-Air.local>
2021-09-23 16:49:20 +0100

    refactored with private methods full? and empty?

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,14 +9,24 @@ class DockingStation
     attr_reader :bike
 
     def release_bike
-        fail "No bikes available" unless @bikes.length > 0
+        fail "No bikes available" unless empty?
         @bikes.pop
     end
 
     def dock(bike)
-        fail "Station is full" if @bikes.length > 19
-
+        fail "Station is full" if full?
         @bikes << bike
         @bike = bike
     end
+
+    private
+
+    def full?
+        @bikes.length > 19
+    end
+
+    def empty?
+        @bikes.length > 0
+    end
+
 end
